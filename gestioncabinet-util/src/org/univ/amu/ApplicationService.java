9db75ecb0e71613dbebe06138e7407699763517e
b21001089 <b21001089@CAN-S109-PC21.salsa.univ-amu.fr>
2014-10-25 11:59:53 +0200

    postcontruct applicationservice

--- gestioncabinet-util/src/org/univ/amu/ApplicationService.java
+++ gestioncabinet-util/src/org/univ/amu/ApplicationService.java
@@ -1,14 +1,21 @@
 package org.univ.amu;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.Properties;
 
+import javax.annotation.PostConstruct;
 import javax.annotation.Resource;
 import javax.ejb.Singleton;
+import javax.ejb.Startup;
 
 import org.jboss.logging.Logger;
 
 
 @Singleton
+@Startup
 public class ApplicationService {
 
 	@Resource(name="org.univ.amu.logger")
@@ -22,5 +29,21 @@ public class ApplicationService {
 	
 	public Logger getLogger(){
 		return log;
+	}
+	
+	@PostConstruct
+	public void initialisation() throws IOException{
+		String path = this.getClass().getResource("").toString();
+		File f = new File(path);
+	
+	
+		if(f.exists()){
+			InputStream p = new FileInputStream(f);
+			props.load(p);
+		}
+		else
+		{
+			this.getLogger().error("Erreur : Le fichier properties n'est pas trouvé");
+		}
 	}
 }

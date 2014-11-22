8f5413f6a2b87f1ac7b07267c1d377a3b9beaa5b
d11618058 <d11618058@CAN6S109-PC20.salsa.univ-amu.fr>
2014-11-22 08:34:41 +0100

    Commit prescriptionservice

--- gestioncabinet-util/src/org/univ/amu/PrescriptionService.java
+++ gestioncabinet-util/src/org/univ/amu/PrescriptionService.java
@@ -1,10 +1,52 @@
 package org.univ.amu;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.ejb.EJB;
 import javax.ejb.LocalBean;
 import javax.ejb.Stateless;
+
+import fr.vidal.webservices.interactionservice.InteractionService;
+import fr.vidal.webservices.productservice.ArrayOfProduct;
+import fr.vidal.webservices.productservice.Product;
+import fr.vidal.webservices.productservice.ProductService;
+import miage.gestioncabinet.api.Interaction;
+import miage.gestioncabinet.api.Produit;
 
 @Stateless
 @LocalBean
 public class PrescriptionService {
 
+	@EJB
+	private ApplicationService appService;
+	
+	@EJB
+	private ProductService prodService;
+	
+	@EJB
+	private InteractionService interactionService;
+	
+	public List<Produit> findProduits(String keyword){
+		List<Produit> lstRetour = new ArrayList<Produit>();
+		try{
+			List<Product> lstProduct = prodService.directSearchByName(keyword).getProduct();
+			for(Product p : lstProduct){
+				Produit prod = (Produit) Class.forName(appService.getProperty("productClass")).newInstance();
+				prod.setCis(p.getCis());
+				prod.setNom(p.getName());
+				lstRetour.add(prod);
+			}
+			return lstRetour;
+		}
+		catch(Exception e){
+			appService.getLogger().error("Erreur lors de la recherche de produit avec le mot clé : "+keyword,e);
+			return new ArrayList<Produit>();
+		}
+	}
+	
+	public List<Interaction> findInteractions(List<Produit> produits){
+		return null;
+	}
+	
 }
